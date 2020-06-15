01410e0740e89a8b4eae3d4810bb41b136a0d283
Matthew McDermott <49165223+SPMatthewMcD@users.noreply.github.com>
2020-06-15 07:21:28 -0500

    Add Test for Valid Token GUID (#17)

--- SpanningO365/Public/Get/Tests/Get-SpanningAuthentication.Functional.Tests.ps1
+++ SpanningO365/Public/Get/Tests/Get-SpanningAuthentication.Functional.Tests.ps1
@@ -27,5 +27,8 @@ Describe 'Get-SpanningAuthentication Functional Tests' {
     It "Get-SpanningAuthentication with Connection returns 1 Header" {
       (Get-SpanningAuthentication -Connection $azConnection).Headers.Count | Should be 1
     }
+    It "Get-SpanningAuthentication fails with malformed token" {
+      {Get-SpanningAuthentication -ApiToken "addc5b8c-1565-4454-aec5-5878544f07" -Region $region -AdminEmail $admin} | Should throw
+    }
   }
 }
--- SpanningO365/SpanningO365.Full.Tests.ps1
+++ SpanningO365/SpanningO365.Full.Tests.ps1
@@ -89,7 +89,6 @@ if ($PublicFunctions.count -gt 0) {
                 }
        }
     }
-
 if ($Error.Count -gt 0)
 {
     Write-Warning "Script Completed with $($Error.Count) error(s)."
