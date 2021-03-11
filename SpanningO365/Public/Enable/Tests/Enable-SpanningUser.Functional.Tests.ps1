23e03c6f54b8c6bc0c5300fd8e4232f6e1efd64e
Matthew McDermott <matthew@ableblue.com>
2021-03-11 10:24:07 -0600

    Planning Tests

--- SpanningO365/Public/Enable/Tests/Enable-SpanningUser.Functional.Tests.ps1
+++
@@ -1,6 +0,0 @@
-#Add Tests for:
-# WhatIf
-# No Users
-# more than 500 users
-# less than 500 users
-# User 404
---
+++ SpanningO365/Public/Enable/Tests/Enable-SpanningUserList.Functional.Tests.ps1
@@ -0,0 +1,6 @@
+#Add Tests for:
+# WhatIf
+# No Users
+# more than 500 users
+# less than 500 users
+# User 404
