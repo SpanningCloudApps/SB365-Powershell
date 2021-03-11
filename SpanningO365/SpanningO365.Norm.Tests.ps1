c88ae0c9cf9b51d53cedfae898889e66a7cceb55
Matthew McDermott <matthew@ableblue.com>
2021-03-11 09:06:30 -0600

    Fixed path query error

--- SpanningO365/SpanningO365.Norm.Tests.ps1
+++ SpanningO365/SpanningO365.Norm.Tests.ps1
@@ -40,7 +40,8 @@ if ($PrivateFunctions.count -gt 0) {
               
            } 
       $function = $AllFunctions.Where{ $_.Name -eq $PrivateFunction.BaseName}
-      $PrivateFunctionTests = $PrivateFunctionstests.Where{$_.Name -match $PrivateFunction.BaseName }
+      # $PrivateFunctionTests = $PrivateFunctionstests.Where{$_.Name -match $PrivateFunction.BaseName }
+      $PrivateFunctionTests = $PrivateFunctionstests.Where{$_.Name -like "$($PrivateFunction.BaseName).*" }
 
             foreach ($PrivateFunctionTest in $PrivateFunctionTests) {
                 . $($PrivateFunctionTest.FullName) $function
@@ -69,7 +70,8 @@ if ($PublicFunctions.count -gt 0) {
             }
             
             $function = $AllFunctions.Where{ $_.Name -eq $PublicFunction.BaseName}
-            $publicfunctionTests = $Publicfunctionstests.Where{$_.Name -match $PublicFunction.BaseName }
+            # $publicfunctionTests = $Publicfunctionstests.Where{$_.Name -match $PublicFunction.BaseName }
+            $publicfunctionTests = $Publicfunctionstests.Where{$_.Name -like "$($PublicFunction.BaseName).*" }
 
             foreach ($publicfunctionTest in $publicfunctionTests) {
                 . $($PublicFunctionTest.FullName) $function
