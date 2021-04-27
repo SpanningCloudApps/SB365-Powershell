38085a14ceb319e406285a1599a2aa06e81e6d0f
Matthew McDermott <matthew@ableblue.com>
2021-04-27 10:41:22 -0500

    Should Be to Should -Be, MyInvocation to PSScriptRoot

--- SpanningO365/SpanningO365.Full.Tests.ps1
+++ SpanningO365/SpanningO365.Full.Tests.ps1
@@ -1,7 +1,5 @@
-﻿# Clear Eror Queue for easier reporting
-$Error.Clear()
-
-$Here = Split-Path -Parent $MyInvocation.MyCommand.Path
+﻿# Pester does not work with $MyInvocation
+$here = $PSScriptRoot
 
 $PrivateFunctions = Get-ChildItem "$here\Private\" -Filter '*.ps1' -Recurse | Where-Object {$_.name -NotMatch "Tests.ps1"}
 $PublicFunctions = Get-ChildItem "$here\Public\" -Filter '*.ps1' -Recurse | Where-Object {$_.name -NotMatch "Tests.ps1"}
@@ -31,14 +29,14 @@ if ($PrivateFunctions.count -gt 0) {
                 $contents = Get-Content -Path $PrivateFunction.FullName -ErrorAction Stop
                 $errors = $null
                 $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
-                $errors.Count | Should Be 0
+                $errors.Count | Should -Be 0
             }
     
     foreach ($rule in $rules) {
 
-                    It “passes the PSScriptAnalyzer Rule $rule“ {
+                    It "Passes the PSScriptAnalyzer Rule $rule" {
 
-                        (Invoke-ScriptAnalyzer -Path $PrivateFunction.FullName -IncludeRule $rule.RuleName ).Count | Should Be 0
+                        (Invoke-ScriptAnalyzer -Path $PrivateFunction.FullName -IncludeRule $rule.RuleName ).Count | Should -Be 0
 
                     }
                  }
@@ -69,13 +67,13 @@ if ($PublicFunctions.count -gt 0) {
                 $contents = Get-Content -Path $PublicFunction.FullName -ErrorAction Stop
                 $errors = $null
                 $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
-                $errors.Count | Should Be 0
+                $errors.Count | Should -Be 0
             }
       foreach ($rule in $rules) {
 
-                    It “passes the PSScriptAnalyzer Rule $rule“ {
+                    It "Passes the PSScriptAnalyzer Rule $rule" {
 
-                        (Invoke-ScriptAnalyzer -Path $PublicFunction.FullName -IncludeRule $rule.RuleName ).Count | Should Be 0
+                        (Invoke-ScriptAnalyzer -Path $PublicFunction.FullName -IncludeRule $rule.RuleName ).Count | Should -Be 0
 
                         }
                     }
